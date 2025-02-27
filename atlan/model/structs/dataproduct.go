b0d79535154052cfc79fc23704d9801e7bf0d5b7
karan <gupta.karan.11@gmail.com>
2025-02-27 17:44:45 +0100

    reading search attribute daapCriticality

--- atlan/model/structs/dataproduct.go
+++ atlan/model/structs/dataproduct.go
@@ -2,6 +2,7 @@ package structs
 
 type DataProduct struct {
 	Asset
-	DataProductDataDomain *DataDomain `json:"dataDomain,omitempty"`
-	DataProductAssetsDSL  *string     `json:"dataProductAssetsDSL,omitempty"`
+	DataProductDataDomain  *DataDomain `json:"dataDomain,omitempty"`
+	DataProductAssetsDSL   *string     `json:"dataProductAssetsDSL,omitempty"`
+	DataProductCriticality *string     `json:"daapCriticality,omitempty"` // One of High, Medium, Low
 }
